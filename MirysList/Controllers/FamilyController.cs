1ee4e93d3d3abb5a19f54dfacebde7570c173a18
richa008 <richa008@gmail.com>
2018-07-21 06:54:18 -0700

    Adding Db Connections

---
+++ MirysList/Controllers/FamilyController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MirysList.Models;
+
+namespace MirysList.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Family")]
+    public class FamilyController : Controller
+    {
+        private AppDbContext _dbContext;
+
+        public FamilyController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/Family
+        [HttpGet]
+        public IEnumerable<Family> Get()
+        {
+            return _dbContext.Families;
+        }
+
+        // POST: api/Family
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+    }
+}
--- MirysList/Startup.cs
+++ MirysList/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using MirysList.Models;
 
 namespace MirysList
 {
@@ -23,11 +25,12 @@ namespace MirysList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DbContextConnectionString")));
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AppDbContext dbContext)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +38,7 @@ namespace MirysList
             }
 
             app.UseMvc();
+            dbContext.Database.EnsureCreated();
         }
     }
 }
