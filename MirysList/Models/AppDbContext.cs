b8b721ea7ab774516882531fd8ec2ba30806f5d0
Swapna Guddanti <swapnag@microsoft.com>
2018-07-23 11:01:32 -0700

    fixing after merging.

--- MirysList/Models/AppDbContext.cs
+++ MirysList/Models/AppDbContext.cs
@@ -15,6 +15,10 @@ namespace MirysList.Models
         }
 
         public DbSet<Family> Families { get; set; }
-
+        public DbSet<Catalog> Catalogs { get; set; }
+        public DbSet<CatalogItem> CataLogItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<List> ShoppingLists { get; set; }
+        public DbSet<ListItem> ShoppingListItems { get; set; }
     }
 }
