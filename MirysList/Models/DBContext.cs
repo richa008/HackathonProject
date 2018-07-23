8272b1dc93764f9f9cd55f56de3509d72a99e7ae
Swapna Guddanti <swapnag@microsoft.com>
2018-07-23 10:46:54 -0700

    adding models for catalog, catalogitem, category, list, listitem

---
+++ MirysList/Models/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MirysList.Models
+{
+	public class MirysListDBContext : DbContext
+    {
+
+        public MirysListDBContext(DbContextOptions<MirysListDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Catalog> Catalogs { get; set; }
+        public DbSet<CatalogItem> CataLogItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<List> ShoppingLists { get; set; }
+        public DbSet<ListItem> ShoppingListItems { get; set; }
+	}
+}
--- MirysList/Startup.cs
+++ MirysList/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MirysList.Models;
 
 namespace MirysList
 {
@@ -24,10 +26,11 @@ namespace MirysList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<MirysListDBContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DbContextConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, MirysListDBContext dbContext)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +38,7 @@ namespace MirysList
             }
 
             app.UseMvc();
+            dbContext.Database.EnsureCreated();
         }
     }
 }
